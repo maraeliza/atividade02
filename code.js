9c754d3c1cd4915045e8f3405db7f8a1fe767fd7
profmara <professorbyjuu@gmail.com>
2024-08-31 21:04:21 -0300

    att

---
+++ code.js
@@ -0,0 +1,54 @@
+$(document).ready(()=>{
+    //esconde todas as divs
+    $("#todasDivs div").hide()
+    $("#SobreDiv").show()
+    //checa se eu cliquei em algum item do menu
+    $("#menu p").click(function(){
+        //pega o id do item
+        var id = $(this).attr('id')
+        //esconde todos os itens
+        $("#todasDivs div").hide()
+        //exibe a div correspondente
+        $("#"+id+"Div").fadeIn()
+        switch(id){
+            case "Sobre":
+                changeColor('green')
+                break
+            case "Equipe":
+                changeColor('blue')
+                break
+            case "Agenda":
+                changeColor('red')
+                break
+            case "Projetos":
+                changeColor('brown')
+                break
+            case "Contato":
+                changeColor('#ffe95c')
+                break
+        }
+    })
+})  
+
+function changeColor(cor){
+    $("#menu").css(
+        "background-color",cor
+    )
+    $("body").css({
+        color:cor
+    })
+    if(cor == "#ffe95c"){
+        
+        $("#menu p").css({
+            color:'orange'
+        })
+        $("body").css({
+            color:'orange'
+        })
+    }else{
+        $("#menu p").css({
+            color:'white'
+        })
+    }
+    
+}
